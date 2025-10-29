197ad53cfee2cd506ccc1ef9bf6638c38fa2a7ce
Agbugba David Chinweuba <57929456+Davidruph@users.noreply.github.com>
2025-10-29 16:56:36 +0100

    fix alignment issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,27 +7,25 @@
 @endsection
 
 @section('content')
-    <section class="bg-white p-5 overflow-hidden">
+    <section class="bg-white flex items-center justify-center overflow-hidden p-5">
         <div
-            class="w-full max-w-[1358px] px-3 md:px-[30px] flex flex-col justify-center items-center 
+            class="w-full max-w-[1358px] flex flex-col justify-center items-center text-center 
                    animate-fade-in-up space-y-3">
 
-            <p
-                class="home-hero-title text-center text-[15px] md:text-[25px] 
+            <p class="home-hero-title text-[15px] md:text-[25px] 
                       opacity-0 animate-fade-in delay-200">
                 Welcome to Golf 4 Community
             </p>
 
             <p
-                class="home-hero-bold-text text-[30px] md:text-[60px] font-bold text-center
-                      opacity-0 animate-fade-in delay-400">
+                class="home-hero-bold-text text-[30px] md:text-[60px] font-bold 
+                      opacity-0 animate-fade-in delay-400 leading-tight">
                 Step Into the Heart of
                 <br class="hidden lg:flex" /> the Game
             </p>
 
-            <div class="-mt-5 lg:-mt-25 md:-mt-10 opacity-0 animate-fade-in-up delay-600">
-                <img src="{{ asset('images/hero.svg') }}" alt="Golf Hero"
-                    class="animate-float mx-auto w-full max-w-[1358px]" />
+            <div class="-mt-5 md:-mt-10 lg:-mt-25 opacity-0 animate-fade-in-up delay-600 w-full flex justify-center">
+                <img src="{{ asset('images/hero.svg') }}" alt="Golf Hero" class="animate-float w-full" />
             </div>
         </div>
     </section>
